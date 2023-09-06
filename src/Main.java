566c58d7369c1f83ec412747820ec961cf4a7aea
Diiaz <dsdijaz@gmail.com>
2023-09-06 15:20:49 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
-public class Main {public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
 
-    int balance = 1000;
-    int amount = balance;
-    if (amount <1000){amount = 0;}
-    int bonuses = amount / 100;
-    int sumbalance = balance + bonuses;
+        int balance = 1000;
+        int amount = balance;
+        if (amount < 1000) {
+            amount = 0;
+        }
+        int bonuses = amount / 100;
+        int sumbalance = balance + bonuses;
 
-    System.out.println("Ваш баланс " + sumbalance + ", вам начислено " + bonuses + " бонусов");
-    // Объявляете переменные для входных данных и
-    // параметров программы: начального счёта,
-    // суммы пополнения и тп
+        System.out.println("Ваш баланс " + sumbalance + ", вам начислено " + bonuses + " бонусов");
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
 
-    // Условным оператором проверяете, превысила ли
-    // сумма пополнения порог, и для этих двух разных
-    // сценариев рассчитываете сумму бонуса и выводите
-    // на экран.
-}
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
 }
